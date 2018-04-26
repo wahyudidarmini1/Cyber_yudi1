d8b6be1dcf3e94180c7128cfac4ed5900d5aafac
wahyudidarmini1 <37975743+wahyudidarmini1@users.noreply.github.com>
2018-04-26 18:33:17 +0700

    Update yudi3.py

--- yudi3.py
+++ yudi3.py
@@ -139,9 +139,16 @@ cctv = {
     "point":{},
     "sidermem":{}
 }
-
-mulai = time.time()
-
+setTime = {}
+setTime = wait2['setTime']
+mulai = time.time() 
+def cms(string, commands): #/XXX, >XXX, ;XXX, ^XXX, %XXX, $XXX...
+    tex = ["+","@","/",">",";","^","%","$","＾","サテラ:","サテラ:","サテラ：","サテラ："]
+    for texX in tex:
+        for command in commands:
+            if string ==command:
+                return True
+    return False
 def restart_program(): 
     python = sys.executable
     os.execl(python, python, * sys.argv)
